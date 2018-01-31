89abe85a8a69f1b8814dea684335a4eca558eace
Amir Behmaram <amirbehmaram4@gmail.com>
2018-01-31 15:34:49 -0800

    Added simple comments, layed out structure of Readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# database-management-system
-Simple database management system in Go
+# __**Database management system by Amir Behmaram**__
+
+### __Installation and setup of GO__
+
+### __Compilation instructions__
+
+### __Running the Program__
+
+### __Supported SQL statements and examples__
+
+#### Databases
+CREATE DATABASE <db name>;
+DROP DATABASE <db name>;
+USE <db name>;
+
+#### Tables
+CREATE TABLE <tb_name> (<name> <type>, <name2> <type2>);
+DROP TABLE <tb_name>;
+SELECT * FROM <tb_name>;
+ALTER TABLE <tb_name> ADD <name> <type>;
--- db_manage_main.go
+++ db_manage_main.go
@@ -1,25 +1,30 @@
 package main
 
+/*     Packages     */
 import "fmt"
 import "os"
 import "bufio"
 
+/*     Main Function     */
 func main() {
+	//Variables
 	scanner := bufio.NewScanner(os.Stdin)
 
+	//Menu
 	fmt.Println("Database Management System by Amir Behmaram")
 	fmt.Println("Currently will perform: ")
 	fmt.Println("	- Database creation & deletion")
 	fmt.Println("	- Table creation, deletion, update, and query")
 	fmt.Println("To exit, type .q")
 
+	//Polling loop
+	fmt.Print("> ")
+	for scanner.Scan() {
 		fmt.Print("> ")
-		for scanner.Scan() {
-			fmt.Print("> ")
-      	line := scanner.Text()
-      	if line == ".q" {
-				fmt.Println("Program ending")
-         	break
-        }
-    }
+      line := scanner.Text()
+      if line == ".q" {
+			fmt.Println("Program ending")
+         break
+      }
+   }
 }
