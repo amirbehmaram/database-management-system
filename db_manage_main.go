acdf5df57b0176fafda9132ec7442d2da67b5aa5
Amir Behmaram <amirbehmaram4@gmail.com>
2018-02-01 22:24:18 -0800

    Testing out github pages with HTML5 Up

--- db_manage_main.go
+++ db_manage_main.go
@@ -5,6 +5,13 @@ import "fmt"
 import "os"
 import "bufio"
 
+/*     Global Constants    */
+var validDbStringsU = []string{"CREATE DATABASE","DROP DATABASE","USE"}
+var validDbStringsL = []string{"create database","drop database","use"}
+
+var validTableStringsU = []string {"CREATE TABLE","DROP TABLE","SELECT * FROM","ALTER TABLE"}
+var validTableStringsL = []string {"create table","drop table","select * from","alter table"}
+
 /*     Main Function     */
 func main() {
 	//Variables
@@ -20,11 +27,47 @@ func main() {
 	//Polling loop
 	fmt.Print("> ")
 	for scanner.Scan() {
-		fmt.Print("> ")
       line := scanner.Text()
       if line == ".q" {
 			fmt.Println("Program ending")
          break
-      }
+      } else {
+			/* Need to break up input line into seperate values:
+			 	check first value, if it exists, begin building execution string
+					else: Bad input error
+				check second value, it it exists, add to exec string
+					else: check if it is a current db name (This is assuming first value
+							was USE otherwise -> bad input error
+				continue checking until ; is hit. If no ; present at end "Missing ; error"
+			*/
+			//Break input into pieces
+			if existsInArr(line[0], validTableStringsL) {
+				fmt.Println("Exists in lower case")
+			}
+
+
+		}
    }
+}
+
+func breakInput(input string) []string {
+	//Temporary array return
+	s := make([]string, 3)
+	s[0] = "temp"
+	s[1] = "temp1"
+
+	return s
+}
+
+func validateString(input string) bool {
+	return true
+}
+
+func existsInArr(testStr string, arr []string) bool{
+	for i := 0; i < len(arr); i++ {
+		if testStr == arr[i] {
+			return true
+		}
+	}
+	return false
 }
