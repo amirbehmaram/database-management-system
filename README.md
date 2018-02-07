378b1a7d898d1c2b5c9c0e515bff726f774b80bb
Amir Behmaram <amirbehmaram4@gmail.com>
2018-02-06 17:46:13 -0800

    Completed parsing and validation of query strings. Skeletons of each statement function are added. Updated Readme with building and running commands

--- README.md
+++ README.md
@@ -5,12 +5,19 @@
 
 ### Compilation instructions
 ----------------------------
+Once go is installed, navigate to the project folder and type
+```go build```
+This will compile the program
 
 ### Running the Program
 -----------------------
+Once the program is built type to run it:
+ ```./database-management-system```
 
 ### Supported SQL statements and examples
 -----------------------------------------
+Currently accepts ALL upper case or lower case versions of the following statements:
+
 #### Databases
 1. CREATE DATABASE <db name>;
 2. DROP DATABASE <db name>;
--- db_manage_main.go
+++ db_manage_main.go
@@ -1,16 +1,19 @@
 package main
 
 /*     Packages     */
-import "fmt"
-import "os"
-import "bufio"
+import (
+	"fmt"
+	"os"
+	"bufio"
+   "strings"
+)
 
 /*     Global Constants    */
-var validDbStringsU = []string{"CREATE DATABASE","DROP DATABASE","USE"}
-var validDbStringsL = []string{"create database","drop database","use"}
+var validDbStringsU = []string{"CREATEDATABASE","DROPDATABASE","USE"}
+var validDbStringsL = []string{"createdatabase", "dropdatabase","use"}
 
-var validTableStringsU = []string {"CREATE TABLE","DROP TABLE","SELECT * FROM","ALTER TABLE"}
-var validTableStringsL = []string {"create table","drop table","select * from","alter table"}
+var validTableStringsU = []string {"CREATETABLE", "DROPTABLE", "SELECT*FROM","ALTERTABLE"}
+var validTableStringsL = []string {"createtable", "droptable","select*from","altertable"}
 
 /*     Main Function     */
 func main() {
@@ -32,42 +35,147 @@ func main() {
 			fmt.Println("Program ending")
          break
       } else {
-			/* Need to break up input line into seperate values:
-			 	check first value, if it exists, begin building execution string
-					else: Bad input error
-				check second value, it it exists, add to exec string
-					else: check if it is a current db name (This is assuming first value
-							was USE otherwise -> bad input error
-				continue checking until ; is hit. If no ; present at end "Missing ; error"
-			*/
-			//Break input into pieces
-			if existsInArr(line[0], validTableStringsL) {
-				fmt.Println("Exists in lower case")
+			// If input length is greater than 0, start query, else output >
+			inputLength := len(line)
+			if inputLength > 0 {
+				// make sure last value is ;
+				if string(line[inputLength - 1]) == ";" {
+
+					// split up text string
+					text := strings.Split(line, " ")
+
+					// get table/db name, will have ; at end
+					varName := text[len(text) - 1]
+
+					// build query string
+					var queryString string
+					for i := 0; i < len(text); i++ {
+						tempStr := text[i]
+
+						if tempStr != varName {
+							queryString += tempStr
+						} else {
+							break
+						}
+					}
+
+					// hack ; off the end of varName
+					if last := len(varName) - 1; last >= 0 && varName[last] == ';' {
+        				varName = varName[:last]
+    				}
+
+					/* Current variable status:
+						queryString has the string to execute "create table" etc.
+						varName has the table of databasename
+					*/
+					valid,message := validQuery(queryString)
+
+					if valid {
+						//Hand off query to either the database or table handler
+						if message == "db" {
+							fmt.Println(databaseHandler(queryString))
+						} else {
+							fmt.Println(tableHandler(queryString))
+						}
+					} else {
+						fmt.Println("....Invalid query string")
+					}
+				} else {
+					fmt.Println("....Error: Missing ; at end of query")
+				}
 			}
-
-
+			fmt.Print("> ")
 		}
    }
 }
 
-func breakInput(input string) []string {
-	//Temporary array return
-	s := make([]string, 3)
-	s[0] = "temp"
-	s[1] = "temp1"
-
-	return s
-}
-
-func validateString(input string) bool {
-	return true
-}
-
 func existsInArr(testStr string, arr []string) bool{
 	for i := 0; i < len(arr); i++ {
-		if testStr == arr[i] {
+		if (testStr == arr[i]) {
 			return true
 		}
 	}
 	return false
+}
+
+func validQuery(queryStr string) (bool,string){
+	if existsInArr(queryStr, validDbStringsL) || existsInArr(queryStr, validDbStringsU) {
+		return true,"db"
+	} else if existsInArr(queryStr, validTableStringsU) || existsInArr(queryStr, validTableStringsL) {
+		return true,"table"
+	} else {
+		return false,"invalid"
+	}
+}
+
+func databaseHandler(queryStr string) string{
+	switch queryStr {
+	case "createdatabase":
+		return createDatabase()
+	case strings.ToUpper("createdatabase"):
+		return createDatabase()
+	case "dropdatabase":
+		return dropDatabase()
+	case strings.ToUpper("dropdatabase"):
+		return dropDatabase()
+	case "use":
+		return useDatabase()
+	case strings.ToUpper("use"):
+		return useDatabase()
+	default:
+		return "....Error, bad database query string"
+	}
+}
+
+func tableHandler(queryStr string) string{
+	switch queryStr {
+	case "createtable":
+		return createTable()
+	case strings.ToUpper("createtable"):
+		return createTable()
+	case "droptable":
+		return dropTable()
+	case strings.ToUpper("droptable"):
+		return dropTable()
+	case "select*from":
+		return selectFrom()
+	case strings.ToUpper("select*from"):
+		return selectFrom()
+	case "altertable":
+		return alterTable()
+	case strings.ToUpper("altertable"):
+		return alterTable()
+	default:
+		return "....Error, bad table query string"
+	}
+}
+
+/*     Database functions     */
+func createDatabase() string{
+	return "Create Database"
+}
+
+func dropDatabase() string{
+	return "Drop Database"
+}
+
+func useDatabase() string{
+	return "Use Database"
+}
+
+/*     Table functions     */
+func createTable() string{
+	return "Create Table"
+}
+
+func dropTable() string{
+	return "Drop Table"
+}
+
+func selectFrom() string{
+	return "Select From Table"
+}
+
+func alterTable() string{
+	return "Alter Table"
 }
